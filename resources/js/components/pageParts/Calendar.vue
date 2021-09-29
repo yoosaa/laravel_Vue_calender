30915dcb064619dc370f51ebefd43f523a75d33b
y-sano <mmyy_125115@gmail.com>
2021-09-29 23:44:34 +0900

    月の表示、移動ボタンの設置

--- resources/js/components/pageParts/Calendar.vue
+++ resources/js/components/pageParts/Calendar.vue
@@ -1,28 +1,49 @@
 <template>
     <div>
-        <v-sheet height="100vh">
+        <v-sheet height="6vh" class="d-flex align-center">
+            <v-btn outlined small class="ma-4" @click="setToday">TODAY</v-btn>
+            <v-btn icon @click="$refs.calendar.prev()">
+                <v-icon>mdi-chevron-left</v-icon>
+            </v-btn>
+            <v-btn icon @click="$refs.calendar.next()">
+                <v-icon>mdi-chevron-right</v-icon>
+            </v-btn>
+            <v-toolbar-title>{{ title }}</v-toolbar-title>
+        </v-sheet>
+        <v-sheet height="94vh">
             <v-calendar
+                ref="calendar"
                 v-model="value"
                 :events="events"
                 @change="fetchEvents"
+                locale="ja-jp"
+                :day-format="(timestamp) => new Date(timestamp.date).getDate()"
+                :month-format="(timestamp) => (new Date(timestamp.date).getMonth() + 1)"
             ></v-calendar>
         </v-sheet>
     </div>
 </template>
 
 <script>
+import {format} from 'date-fns';
 import {mapGetters, mapActions} from 'vuex';
 
 export default {
     name: 'Calendar',
     data: () =>({
-        value: new Date('2021/09/01'),
+        value: format(new Date(), 'yyyy/MM/dd'),
     }),
     computed: {
         ...mapGetters('events', ['events']),
+        title() {
+            return format(new Date(this.value), 'yyyy/MM/dd');
+        }
     },
     methods: {
-        ...mapActions('events', ['fetchEvents'])
+        ...mapActions('events', ['fetchEvents']),
+        setToday() {
+            this.value = format(new Date(), 'yyyy/MM/dd')
+        }
     }
 }
 </script>
